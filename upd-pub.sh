cbaf3083d62d21a386b719cd3fb129868e9de627
Jan Heiland <jnhlbd@gmail.com>
2019-11-21 20:45:59 +0100

    beh-bh-19

--- upd-pub.sh
+++ upd-pub.sh
@@ -1,3 +1,3 @@
 # academic import --overwrite --bibtex ~/work/misc/bibfiles/mybibfiles/mypublist/mp.bib
-academic import --bibtex ~/work/misc/bibfiles/mybibfiles/mypublist/mp.bib
+academic import --bibtex ~/texmf/bibtex/bib/mybibfiles/mypublist/mp.bib
 find content/publication/ -type f -exec sed -i '/^publishDate/d' {} \;
